7300e42933a220e5726202585e7f6241ef664026
2018161062Jiseong <106158534+2018161062Jiseong@users.noreply.github.com>
2022-05-25 10:03:50 +0900

    Add files via upload

---
+++ ChildView.cpp
@@ -0,0 +1,69 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "ColorSelect.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+	m_color = RGB(255, 0, 0);
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+	CPaintDC dc(this); 
+
+	CFont font;
+	font.CreatePointFont(300, _T("궁서"));
+	dc.SelectObject(&font);
+	dc.SetTextColor(m_color);
+
+	CRect rect;
+	GetClientRect(&rect);
+	CString str = _T("메뉴 테스트");
+	dc.DrawText(str, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+
+	
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+	
+	// 그리기 메시지에 대해서는 CWnd::OnPaint()를 호출하지 마십시오.
+}
+
+
+
